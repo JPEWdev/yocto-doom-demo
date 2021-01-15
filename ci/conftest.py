fcb19d442e7fb2cd0006bd3e53ae4ac17fac91cc
Joshua Watt <JPEWhacker@gmail.com>
2021-01-15 08:43:18 -0600

    ci: Assign IPv6 address
    Check if the device already has the required IPv6 address assigned and
    if not assign it.

--- ci/conftest.py
+++ ci/conftest.py
@@ -1,7 +1,15 @@
+import json
 import pytest
 import time
 from labgrid.protocol import CommandProtocol
-from labgrid.driver import USBStorageDriver, USBSDWireDriver, USBSDMuxDriver, Mode, SSHDriver
+from labgrid.driver import (
+    USBStorageDriver,
+    USBSDWireDriver,
+    USBSDMuxDriver,
+    Mode,
+    SSHDriver,
+)
+from labgrid.resource import NetworkService
 
 
 @pytest.fixture(scope="function")
@@ -16,11 +24,47 @@ def shell_command(target, strategy, capsys):
         strategy.transition("shell")
     return target.get_active_driver(CommandProtocol)
 
+
 @pytest.fixture(scope="function")
 def ssh_command(target, strategy, capsys):
     with capsys.disabled():
         strategy.transition("shell")
+    shell = target.get_active_driver(CommandProtocol)
+
+    net = target.get_resource(NetworkService)
+    address = net.address.split("%", 1)[0]
+
+    interfaces = json.loads("\n".join(shell.run_check("ip -j addr")))
+
+    assign_intf = None
+    found = False
+    for intf in interfaces:
+        if not intf["ifname"].startswith("eth") and not intf["ifname"].startswith("en"):
+            continue
+
+        for addr in intf["addr_info"]:
+            if addr["family"] == "inet6" and addr["local"] == address:
+                found = True
+                break
+
+        if found:
+            break
+
+        if assign_intf is None and intf["operstate"] == "UP":
+            assign_intf = intf
+
+    if not found:
+        if assign_intf is not None:
+            shell.run_check(
+                "ip addr add dev %s scope link %s" % (assign_intf["ifname"], address)
+            )
+            # Wait for device to configure services
+            time.sleep(3)
+        else:
+            pytest.fail("No active interface found to assign address %s" % address)
+
     return target.get_driver(SSHDriver)
+
 
 @pytest.fixture(scope="session")
 def image(target, strategy):
